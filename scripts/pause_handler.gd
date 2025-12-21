06172cfcffcd47efc9bcc67db7e1d197cd02eadb
Ben Carey <benjamin.james.carey3@gmail.com>
2025-12-20 21:59:33 -0500

    input: adds pausing
    Game now defaults to capturing the mouse. Press "Esc" to unbind the
    mouse and pause the game, and "Esc" again to recapture the mouse and
    resume play.

---
+++ scripts/pause_handler.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _process(_dt: float) -> void:
+	if get_tree().get_nodes_in_group("world") == []:
+		return
+	var world: Node3D = get_tree().get_nodes_in_group("world")[0]
+	if Input.is_action_just_pressed("pause"):
+		if world.process_mode == Node.PROCESS_MODE_DISABLED:
+			world.process_mode = Node.PROCESS_MODE_INHERIT
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		else:
+			world.process_mode = Node.PROCESS_MODE_DISABLED
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
